015219927e31df0b931b586c8ffb02174000dcea
hugolesta <mov10.hnl22@gmail.com>
2025-03-22 17:26:21 +0100

    fix: Fixes lazy repository url

--- nvim/lua/hugolesta/lazy.lua
+++ nvim/lua/hugolesta/lazy.lua
@@ -1,10 +1,10 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then 
+if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-   "git",
+    "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.vim.git",
+    "https://github.com/folke/lazy.nvim.git",
     "branch=stable", -- Latest stable release.
     lazypath,
   })
