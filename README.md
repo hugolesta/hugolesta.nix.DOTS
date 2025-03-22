a4021becf0e0a33384c137338a132daf49696341
hugolesta <mov10.hnl22@gmail.com>
2025-03-22 16:58:56 +0100

    feat: Adds nix dots for container devs

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hugo Lesta DOTS
+
+This Repository aims to have all the configuration files I'm using on daily basis. 
+
---
+++ config.nix
@@ -0,0 +1,21 @@
+{
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "hugolesta-tools";
+      paths = [
+        zsh-completions
+        neovim
+        go
+        nodejs_22
+        pure-prompt
+        fd
+        ripgrep
+        fzf
+        lazygit
+        kubectl
+        kubectx
+        k9s
+      ];
+    };
+  };
+}
